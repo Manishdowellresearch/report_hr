85ac3bf85f4ebfea9fe777ee4d4d51fdf13eef4a
Manish !! <mdashsharma95@outlook.com>
2022-08-06 04:27:27 +0530

    report.html and home.html

--- report_hr/urls.py
+++ report_hr/urls.py
@@ -18,5 +18,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('report/',include('report_hr_hiring.urls'))
+    path('',include('report_hr_hiring.urls'))
 ]
--- report_hr_hiring/views.py
+++ report_hr_hiring/views.py
@@ -17,7 +17,15 @@ def home(request):
 def generate_report(request):
     if request.method == 'POST':
         candidate_name =request.POST['candidate_name']
-        return HttpResponse(candidate_name)
+        response = targeted_population('hr_hiring','tasks',  ['task_details'], 'life_time')
+        all_tasks = [data['task_details'] for data in response['normal']['data'][0]]
+        def find_candidate_task(candidate, task_list):
+            for task in task_list:
+                if task['user'] == candidate:
+                    return task
+            return {"message": f"No task for {candidate} found"}
+        candidate_task = find_candidate_task(candidate_name, all_tasks)
+        return render(request, 'report.html',context={"candidate_task":candidate_task})
     else:
         return HttpResponse("ok")
         #response = targeted_population('hr_hiring','tasks',  ['task_details'], 'life_time')
